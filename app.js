4e9d9b0781a3b9cd1d1f25814eca967bc200cc27
caoXueLiang <2357747972@qq.com>
2020-01-22 17:52:01 +0800

    电站详情

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
+/*
+ * @Author: your name
+ * @Date: 2020-01-15 14:20:52
+ * @LastEditTime: 2020-01-21 17:12:04
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /weiweixing-miniprogram/app.js
+ */
 App({
   onLaunch: function () {
+    const updateManager = wx.getUpdateManager()
 
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      console.log(res.hasUpdate)
+    })
+    
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success(res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+    
+    updateManager.onUpdateFailed(function () {
+      // 新版本下载失败
+    })
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your title
  * @Date: 2020-01-15 14:20:52
- * @LastEditTime : 2020-01-21 14:21:39
+ * @LastEditTime : 2020-01-21 17:41:51
  * @LastEditors  : Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /weiweixing-miniprogram/index/index.js
@@ -24,6 +24,7 @@ Page({
     fiterDistance:'10km',
     fiterChargeType:'充电站',
     fiterPerference:'偏好',
+    chargePoleDetail:{"data":{"stationId":"a01b7b3b-2d62-428e-a635-b6b587689b44","stationName":"北京酒仙桥语言文化中心充电站","fastTerminalNum":5,"slowTerminalNum":5,"fastTerminalIdleNum":3,"slowTerminalIdleNum":4,"outNetworkTerminalNum":0,"usingTerminalNum":3,"isPersonalTerminal":false,"isTeldPersonalTerminal":false,"defaultImg":"https://resource.teld.cn/teldimage/115/9ce32d9e4b8c468bae8166a9cd3e6595_w200h200.jpg","imageSize":5,"advertisementImg":"","advertisementHtml":"","positionimgs":["https://resource.teld.cn/teldimage/115/badfc6d75c43442780a721bec19b2e9e.jpg"],"hasLock":"N","collected":"N","stationAddress":"北京市市辖区朝阳区北京市朝阳区将台西路2号","positionDesc":"酒仙桥语言文化中心（毗邻北京日本人学校西北侧）项目B2层089-094车位","lng":116.475639,"lat":39.975114,"distance":"2.9km","businessHours":"周一至周日00:00-24:00","operatorPhone":"4001-300-001","operatorName":"北汽特来电（北京）新能源科技有限公司","parkFee":"免费","priceDesc":"2.00元/度","specialDesc":"","payType":"本APP，特来电微信、支付宝小程序","specialDescV3":"","isInterconnection":false,"score":"3.9","priceScore":"--","serviceScore":"--","environmentScore":"--","fastParkIdleNum":"0","slowParkIdleNum":"0","fastParkNum":"0","slowParkNum":"0","canOrderFastNum":"0","canOrderSlowNum":"0","supportOrderFastNum":"0","supportOrderSlowNum":"0","openAir":"0","floor":"B2","highLimit":"","notBusinessHour":false,"originalPrice":"","limitChargeDesc":"","isRecommend":true,"timeoutFeeDesc":"","rangeTime":"00:00-24:00","rangePrice":"2.0000元/度","originalRangePrice":"","rangeSize":1,"nextRangeTime":"","nextRangePrice":"","electricPrice":"1.2000元/度","servicePrice":"0.8000元/度","lastChargeTime":"25分钟前","sceneManageName":"北汽特来电","sceneManageMobile":"","newStationType":"01","originalElectricPrice":"","originalServicePrice":"","mapImage":"","tagsNew":[{"tagLabel":"自营","tagColor":"8c8c8c","tagDesc":""},{"tagLabel":"对外开放","tagColor":"8c8c8c","tagDesc":""},{"tagLabel":"非露天B2","tagColor":"8c8c8c","tagDesc":""},{"tagLabel":"免费停车","tagColor":"8c8c8c","tagDesc":""},{"tagLabel":"芝麻信用·免充值","tagColor":"1CCFC9","tagDesc":""}],"lockDevicePhone":"","cameraIdentify":false,"carNoDiscountRule":"","pileOwner":[],"qaSupport":false,"commentSupport":true,"invoiceSupport":"特来电新能源有限公司","entrances":[{"lng":"116.475639","lat":"39.975114","name":"停车场入口"}],"tempCarTypeTags":[],"stationService":[{"name":"室内","img":"http://resource.teld.cn/teldimage/0/9a7e552a43b3408280954ff9e5ed7919.png","url":""}],"plusPrice":"","exclusivePrice":"","operateCompanyID":"52e810c1-42fd-46a8-9ea2-59b79524ba9b","settleCompanyID":"52e810c1-42fd-46a8-9ea2-59b79524ba9b","stationServiceDesc":"楼内有洗手间","businessDistrict":"朝阳区","aComment":"","parkFeeDesc":"停车免费","operateState":"3","monitorURL":"","parkfeeDiscount":false,"navPoints":[{"nav":[{"type":"2","name":"毗邻北京日本人学校西北侧","lng":"116.475639","lat":"39.975114"}]}],"autoLowerLock":false,"barrierLimit":false,"parkfeeDiscountType":"","priceInfo":[]},"errcode":null,"errmsg":null,"state":"1"},
     fiterDistanceIsSelected:false,
     fiterChargeTypeIsSelected:false,
     fiterPerferenceIsSelected:false,
@@ -257,6 +258,35 @@ Page({
  confirmMenthod(){
   this.hideWindow();
  },
+
+ /**
+  *使用微信内置地图查看位置
+  *
+  * @param {*} e
+  */
+ daohangMenthod(e){
+  const latitude = Number(e.currentTarget.dataset.item.location.lat);
+  const longitude = Number(e.currentTarget.dataset.item.location.lng);
+  let name = e.currentTarget.dataset.item.title;
+  let address = e.currentTarget.dataset.item.address;
+  wx.openLocation({
+    latitude,
+    longitude,
+    name,
+    address,
+    scale: 18
+  })
+ },
+
+ /**
+  *充电桩详情
+  *
+  */
+ goToDetailMenthod(){
+    wx.navigateTo({
+      url: '../chargePileDetail/chargePileDetail',
+    })
+ },
  
  /**
   * 小程序调用扫码
@@ -371,5 +401,15 @@ Page({
     this.setData({
       showBottom:false,
     })
-  }
+  },
+  
+   /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function (res) {
+    return {
+      title: '仿写特来电',
+      path: '/pages/index/index'
+    }
+  },
 })
